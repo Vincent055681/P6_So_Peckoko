b51d38ae0f093a914884b581e4dc4813e22dc015
vincent <to@toto.fr>
2021-06-27 22:12:34 +0200

    correction du path de api.use ou il manquait un espace, empechant l'application de fonctionner. Ajout du code permetant de se connecter a un compte existant, ajout de la fonctionalité pour créer un compte dans la BDD mongo, et sauce.js dans le dossier routes à un code commenté qui correpsond au début du code des routes pour les sauces

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const router = express.Router()
+
+const sauceCtrl = require('../controllers/sauce')
+
+// router.get("/", sauceCtrl.XXX)
+// router.get("/:id", sauceCtrl.XXX)
+// router.post("/", sauceCtrl.XXX)
+// router.put("/:id", sauceCtrl.XXX)
+// router.delete("/:id", sauceCtrl.XXX)
+
+
+
+
+module.exports = router
