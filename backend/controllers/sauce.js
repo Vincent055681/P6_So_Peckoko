85a459803946c7c7295d354156cf0d463b1b10c0
vincent <to@toto.fr>
2021-07-02 11:51:57 +0200

    Utilisation de switch case pour la gestion des likes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -56,37 +56,48 @@ exports.deleteSauce = (req, res, next) => {
 }
 
 exports.likeSauce = (req, res, next) => {
-
   let like = req.body.like
   let userId = req.body.userId
   let sauceId = req.params.id
-  console.log(req.body);
-  console.log(req.params);
+  
+  switch (like) {
 
-  if (like === 1) { 
-    Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 }})
-      .then(() => res.status(200).json({ message: `J'aime` }))
-      .catch((error) => res.status(400).json({ error }))
-  }
-  if (like === -1) {
-    Sauce.updateOne({ _id: sauceId }, { $push: { usersDisliked: userId }, $inc: { dislikes: +1 }})
-      .then(() => { res.status(200).json({ message: `Je n'aime pas` }) })
-      .catch((error) => res.status(400).json({ error }))
-  }
-  if (like === 0) {
-    Sauce.findOne({ _id: sauceId })
-       .then((sauce) => {
-        if (sauce.usersLiked.includes(userId)) { 
-          Sauce.updateOne({ _id: sauceId }, { $pull: { usersLiked: userId }, $inc: { likes: -1 }})
-            .then(() => res.status(200).json({ message: `Neutre` }))
-            .catch((error) => res.status(400).json({ error }))
-        }
-        if (sauce.usersDisliked.includes(userId)) { 
-          Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }})
-            .then(() => res.status(200).json({ message: `Neutre` }))
-            .catch((error) => res.status(400).json({ error }))
-        }
-      })
-      .catch((error) => res.status(404).json({ error }))
+    case 1 :
+      if (like === 1) { 
+        Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 }})
+          .then(() => res.status(200).json({ message: `J'aime` }))
+          .catch((error) => res.status(400).json({ error }))
+      }      
+      break;
+
+    case 0 :
+      if (like === 0) {
+        Sauce.findOne({ _id: sauceId })
+           .then((sauce) => {
+            if (sauce.usersLiked.includes(userId)) { 
+              Sauce.updateOne({ _id: sauceId }, { $pull: { usersLiked: userId }, $inc: { likes: -1 }})
+                .then(() => res.status(200).json({ message: `Neutre` }))
+                .catch((error) => res.status(400).json({ error }))
+            }
+            if (sauce.usersDisliked.includes(userId)) { 
+              Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 }})
+                .then(() => res.status(200).json({ message: `Neutre` }))
+                .catch((error) => res.status(400).json({ error }))
+            }
+          })
+          .catch((error) => res.status(404).json({ error }))
+      }
+      break;
+
+    case -1 :
+      if (like === -1) {
+        Sauce.updateOne({ _id: sauceId }, { $push: { usersDisliked: userId }, $inc: { dislikes: +1 }})
+          .then(() => { res.status(200).json({ message: `Je n'aime pas` }) })
+          .catch((error) => res.status(400).json({ error }))
+      }
+      break;
+      
+      default:
+        console.log(error);
   }
 }
