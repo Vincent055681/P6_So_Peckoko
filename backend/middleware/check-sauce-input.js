2ec1a4488526cc4ce8d4715a2a044a908d0a72ea
vincent <to@toto.fr>
2021-07-13 22:21:54 +0200

    Le middleware pour vérifier les input des sauces marche bien sauf quand on update la sauce, je sais pas pourquoi. Parcontre le middleware pour vérifier le mail marche bien. Reste donc à faire marcher la route put et ajouter les user à MongoDB

--- backend/middleware/check-sauce-input.js
+++ backend/middleware/check-sauce-input.js
@@ -1,13 +1,16 @@
 module.exports = (req, res, next) => {
     const sauce = JSON.parse(req.body.sauce)
     let {name, manufacturer, description, mainPepper} = sauce;
-
-    const toTrim = (...string) => {
-        string = string.trim()
-    }
+    let trimedTab = []
+    
+    function toTrim(...string) {
+         trimedTab = string.map((elt) => elt.trim());
+    } 
+    
     toTrim(name, manufacturer, description, mainPepper)
+    console.log(trimedTab);
 
-    if (name.length >= 3 && manufacturer.length >= 3 && description.length >= 3 && mainPepper.length >= 3 ) {
+    if (trimedTab[0].length >= 3 && trimedTab[1].length >= 3 && trimedTab[2].length >= 3 && trimedTab[3].length >= 3 ) {
         next()
     } else {
         throw new Error("Tous les champs doivent faire au moins 3 caractères")
