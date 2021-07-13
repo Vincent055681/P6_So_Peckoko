175462c786f05f46d96c915bc5ced1621b6a7ca9
vincent <to@toto.fr>
2021-07-13 20:47:36 +0200

    La vérification des entrée dans les sauces est fonctionnelle même si pas très belle

--- backend/middleware/check-sauce-input.js
+++ backend/middleware/check-sauce-input.js
@@ -1,19 +1,15 @@
 module.exports = (req, res, next) => {
     const sauce = JSON.parse(req.body.sauce)
-    console.log(sauce);
     let {name, manufacturer, description, mainPepper} = sauce;
 
     const toTrim = (...string) => {
         string = string.trim()
     }
     toTrim(name, manufacturer, description, mainPepper)
-    console.log(name, manufacturer, description, mainPepper)
 
     if (name.length >= 3 && manufacturer.length >= 3 && description.length >= 3 && mainPepper.length >= 3 ) {
-        console.log("ok")
         next()
     } else {
-        console.log('non');
         throw new Error("Tous les champs doivent faire au moins 3 caractères")
     }
   };
