16764de53a6c77a15a3b013bc90546cbb7a15056
vincent <to@toto.fr>
2021-07-14 12:15:26 +0200

    Version finale

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const multer = require("../middleware/multer-config");
 const auth = require("../middleware/auth");
 const checkSauceInput = require("../middleware/check-sauce-input")
-const checkSauceInput2 = require("../middleware/check-sauce-input2")
 
 const sauceCtrl = require("../controllers/sauce");
 
 router.get("/", auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.post("/", auth, multer, checkSauceInput, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, checkSauceInput2, sauceCtrl.updateSauce)
+router.put("/:id", auth, multer, checkSauceInput, sauceCtrl.updateSauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 router.post("/:id/like", auth, sauceCtrl.likeDislikeSauce)
 
